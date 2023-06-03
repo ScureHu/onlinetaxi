27eb4f940d3ecd049acab1948f0c843c0540a69c
huruilei <chh15068288171@outlook.com>
2023-06-03 19:58:28 +0800

    feat:初始化项目

---
+++ eureka/src/main/java/com/dolphin/EurekaApplication.java
@@ -0,0 +1,13 @@
+package com.dolphin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        new SpringApplication(EurekaApplication.class).run(args);
+    }
+}
